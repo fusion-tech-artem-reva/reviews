1892d11bbc95956b5c3e42afa36ebfbfb0d51342
fusion-tech-artem-reva <artem.reva@fusion-tech.pro>
2023-08-09 23:24:30 +0300

    Commi1 (test_1)

---
+++ test.js
@@ -0,0 +1,12 @@
+const parseString = (string) => {
+  const valueToParse = string.trim();
+  const result = [];
+  let maxLength = 140 - 4;
+
+  if (valueToParse.length <= 140) {
+    return [valueToParse];
+  }
+
+  
+
+}
